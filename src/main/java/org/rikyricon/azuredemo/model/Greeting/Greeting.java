f61017a96e9dfc26a202667c581e2826249caff4
ricardo8a <ricardo.diaz.8a@gmail.com>
2023-10-23 18:18:01 -0600

    add greeting controller

---
+++ src/main/java/org/rikyricon/azuredemo/model/Greeting/Greeting.java
@@ -0,0 +1,3 @@
+package org.rikyricon.azuredemo.model.Greeting;
+
+public record Greeting(long id, String content) { }
---
+++ src/test/java/org/rikyricon/azuredemo/AzuredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rikyricon.azuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
